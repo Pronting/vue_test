4cf95b337cfa664ab8eb8c8668b9bd5cecc7ac49
pronting <2233192735@qq.com>
2022-11-21 15:17:43 +0800

    111

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
+/**
+ * ? 该文件是整个文件的入口文件
+ */
 
+// ? 引入Vue
+import Vue from 'vue';
+// ? 引入App组件，她是所有组件的父组件
+import App from './App.vue'
+// ? 关闭Vue的生产提示
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  // ? 将App组件放入了容器中
+  // template: '<h1>你好啊</h1>',
+  components: {App},
+  render:h => h(App)
 }).$mount('#app')
