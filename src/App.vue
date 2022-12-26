549f356b2a8c7698c0537f6f9d7b8a7ed85e56f8
pronting <2233192735@qq.com>
2022-12-26 11:37:56 +0800

    router

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,41 @@
 <template>
-	<div>
-    <Count/>
-    <hr/>
-    <Person/>
-	</div>
+  <div class="container mt-4">
+    <button>原生的按钮</button>
+    <input type="text"/>
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row>
+    <div class="block row">
+      <span class="demonstration">带快捷选项</span>
+      <el-date-picker
+          v-model="value2"
+          type="datetime"
+          placeholder="选择日期时间"
+          align="right"
+          :picker-options="pickerOptions">
+      </el-date-picker>
+    </div>
+    <el-row>
+      <el-button icon="el-icon-search" circle></el-button>
+      <el-button type="primary" icon="el-icon-edit" circle></el-button>
+      <el-button type="success" icon="el-icon-check" circle></el-button>
+      <el-button type="info" icon="el-icon-message" circle></el-button>
+      <el-button type="warning" icon="el-icon-star-off" circle></el-button>
+      <el-button type="danger" icon="el-icon-delete" circle></el-button>
+    </el-row>
+  </div>
+
 </template>
 
 <script>
-import Count from "@/components/Count";
-import Person from "@/components/Person";
-	export default {
-		name:'App',
-		components:{Count,Person},
-    data(){
-		  return{
 
-      }
-    },
-	}
+export default {
+  name: 'App',
+  components: {},
+}
 </script>
-<style>
-
-</style>
--- src/main.js
+++ src/main.js
@@ -2,20 +2,23 @@
 import Vue from 'vue'
 //引入App
 import App from './App.vue'
-//引入store
-import store from './store'
+//完整引入
+//引入ElementUI组件库
+// import ElementUI from 'element-ui';
+//引入ElementUI的全部样式
+// import 'element-ui/lib/theme-chalk/index.css';
 //关闭Vue的生产提示
+//按需引入
+import { Button,Row,DatePicker } from 'element-ui';
 Vue.config.productionTip = false
-
-
-
+Vue.component(Button.name,Button)
+Vue.component(Row.name,Row)
+Vue.component(DatePicker.name,DatePicker)
+//应用ElementUI
+// Vue.use(ElementUI);
+//应用插件
 //创建vm
-const vm = new Vue({
-	el:'#app',
-	store,
-	render: h => h(App),
-	beforeCreate() {
-		Vue.prototype.$bus = this
-	},
+new Vue({
+    el: '#app',
+    render: h => h(App)
 })
-console.log(vm)
